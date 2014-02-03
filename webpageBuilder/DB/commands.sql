d65ff5d0fc4502c35b25f6280021252a4904152a
ofa1 <omair.ahmed@gmail.com>
2014-02-03 21:06:07 +0530

    Initial SQL commands

---
+++ webpageBuilder/DB/commands.sql
@@ -0,0 +1,5 @@
+create database webpageBuilder;
+use webpagebuilder;
+
+create table webpage (PageID numeric, Username varchar(20), title varchar(20), description varchar(1000), paragraphs text, 
+						formID varchar(), images varchar(200))
