321a4bdbf7742da1b096e55e4cab26ac9f877b4e
akhil29 <akhil.appana@gmail.com>
2014-02-04 12:01:12 +0530

    started todb.java

--- webpageBuilder/DB/commands.sql
+++ webpageBuilder/DB/commands.sql
@@ -1,5 +1,6 @@
 create database webpageBuilder;
 use webpagebuilder;
 
-create table webpage (PageID numeric, Username varchar(20), title varchar(20), description varchar(1000), paragraphs text, 
-						formID varchar(), images varchar(200))
+create table webpage (pageid int AUTO_INCREMENT, title varchar(20), description varchar(1000), paragraphs text,primary key (pageid));
+						
+create table pagedata(pageid int,elementtype varchar(20),elementid int,seq int,FOREIGN KEY (pageid) REFERENCES webpage(pageid) ON DELETE CASCADE);
